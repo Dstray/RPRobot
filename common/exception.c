b589a5a732c2bfc2da82ddf42a0ad6b59473838c
CarbonCook <lixt12@126.com>
2016-02-02 21:26:41 +0800

    update

--- common/exception.c
+++ common/exception.c
@@ -6,12 +6,12 @@
 
 #include "exception.h"
 
-static void errno_exit(const char* s) {
+void errno_exit(const char* s) {
     fprintf(stderr, "%s error %d, %s\n", s, errno, strerror(errno));
     exit(EXIT_FAILURE);
 }
 
-static void exception_exit(const char* s1, const char* s2) {
+void exception_exit(const char* s1, const char* s2) {
     fprintf(stderr, "Exception: %s %s\n", s1, s2);
     exit(EXIT_FAILURE);
 }

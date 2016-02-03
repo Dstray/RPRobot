4fdd4a90ac9943949788aa945774262068fece73
CarbonCook <lixt12@126.com>
2016-02-03 18:35:40 +0800

    update video/capture.c

--- common/exception.h
+++ common/exception.h
@@ -12,6 +12,7 @@
 
 #define errno_report(s) fprintf(stderr, "%s error %d, %s\n", s, errno, strerror(errno))
 #define exception_report(s) fprintf(stderr, "Exception: %s\n", s)
+#define exception_report(s1, s2) fprintf(stderr, "Exception: %s %s\n", s1, s2)
 
 extern void errno_exit(const char* s);
 extern void exception_exit(const char* s1, const char* s2);
--- video/capture.c
+++ video/capture.c
@@ -45,14 +45,14 @@ void init_device(int fd, const char* dev_name, enum io_method io) {
             exception_report("struct v4l2_cropcap type is invalid");
         else
             errno_report("VIDIOC_CROPCAP");
-    }
+    } else fprintf(stdio, "Check the cropping limits OK\n");
     // Set the current cropping rectangle
     struct v4l2_crop crop;
     crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     crop.c = cropcap.defrect;
     if (xioctl(fd, VIDIOC_S_CROP, &crop) == -1) {
         if (errno == EINVAL)
-            exception_report("Cropping is not supported");
+            exception_report(dev_name, "does not support cropping");
         else
             errno_report("VIDIOC_S_CROP");
     }
